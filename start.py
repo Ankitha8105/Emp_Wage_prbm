b9d50215b19f52806b63ceae2637d26ea6fcc418
Ankitha8105 <ankithabl2001@gmail.com>
2024-11-22 16:24:46 +0530

    [Ankitha] Add:python program to display print message

---
+++ start.py
@@ -0,0 +1,22 @@
+'''
+    @Author:Ankitha
+    @Date: 22-11-2024
+    @Last Modified by: Ankitha
+    @Last Modified time: 22-11-2024
+    @Title : Employee-Wage computation problem
+'''
+
+def display():
+    """
+    Description:
+        This functtion to display print statment
+    Returns:
+        None
+    """
+    print("Welcome to Employee Wage Computation Program !!!")
+
+def main():
+    display()
+
+if __name__ == "__main__":
+    main()
