28439b97b1a7faeffdd7a714f2977f6729ece9c7
Ankitha8105 <ankithabl2001@gmail.com>
2024-11-22 16:37:33 +0530

    [Ankitha] Add:Python program to check employee is present or absent

--- start.py
+++ start.py
@@ -5,6 +5,7 @@
     @Last Modified time: 22-11-2024
     @Title : Employee-Wage computation problem
 '''
+import random
 
 def display():
     """
@@ -15,8 +16,25 @@ def display():
     """
     print("Welcome to Employee Wage Computation Program !!!")
 
+def check_attendence(num_emp):
+    """Description: 
+            Function to print employee is present or not
+        Parameters:
+            num_emp : Number of employees
+        Returns:
+            Employee is present or not
+        """
+    for emp in range(1,num_emp+1):
+        rand_val = random.randint(0,1)
+        if(rand_val == 1):
+            print(f"Employee{emp} is present")
+        else:
+            print(f"Employee{emp} is Not present")
+
 def main():
     display()
+    num_emp = int(input("Enter the total number of employees :"))
+    check_attendence(num_emp)
 
 if __name__ == "__main__":
     main()
