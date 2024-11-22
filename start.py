c7913847ce745228576bf6403363ef51d330b6a1
Ankitha8105 <ankithabl2001@gmail.com>
2024-11-22 17:13:36 +0530

    [Ankitha] Modified:Python program to calculate pert time  employee wage

--- start.py
+++ start.py
@@ -43,6 +43,15 @@ def cal_wage(hr_wage,total__hr):
     return wage_per_day
 
 def parttime_wage(parttime_hr,hr_wage):
+    """
+        Description: 
+            Function to calculate part time employee wage 
+        parameters:
+            hr_wage: per hour wage
+            parttime_hr:total number of hours
+        Returns:
+            returns total amount of part time employee wage
+    """
     return parttime_hr*hr_wage
 
 def main():
